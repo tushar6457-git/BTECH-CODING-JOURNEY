101f23944c872c7f212440c4f9ebfaab89d144c0
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-05-21 15:19:26 +0530

    Added multiple c files

---
+++ c/simpleinterest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float p = 100;
+    float r = 10;
+    float t = 2;
+
+    float si = (p*r*t)/100;
+    printf("Simple Interest is %f", si);
+
+    return 0;
+}
---
+++ c/userinput.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int p,q ;
+    printf("Enter values of p and q ");
+    scanf("%d%d",&p,&q);
+    printf("p = %d and q = %d\n",p,q);
+    return 0;
+}
