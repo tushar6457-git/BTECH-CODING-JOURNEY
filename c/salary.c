5843dbbda1814e0293f6bcb01ca1c9688ec8285e
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-05-24 23:10:35 +0530

    Updated files: deleted, renamed, modified C files

--- c/salary.c
+++ c/salary.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 int main(){
 
-    int x,y;
-    printf("Enter :");
-    scanf("%d",&x);
-    
+    int cp,sp;
+    printf("Enter Cost Price of the product:");
+    scanf("%d",&cp);
+
+    printf("Enter Selling Price of the Product:");
+    scanf("%d",&sp);
+
+    if(sp>cp){
+        printf("Its Profit!!!\n");
+    }
+    if(cp>sp){
+        printf("Its Loss:)\n");
+    }
+    if(cp==sp){
+        printf("Its Same...\n");
+    }
     return 0;
 }
