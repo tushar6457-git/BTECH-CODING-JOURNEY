217afccf5ca771a4a4cf0877f027510cd6598c28
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-05-26 22:55:43 +0530

    Added multiple C files

---
+++ c/basicofLoops.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    // printf("Hey Dude!\n");
+    // printf("Hey Dude!\n");
+    // printf("Hey Dude!\n");
+    // printf("Hey Dude!\n");
+    for(int i=1;i<=99;i++){
+        printf("Hey Dude!\n");
+    }
+    return 0;
+}
---
+++ c/theRealthing.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a;
+    if(3 + 2%5){
+        printf("This Works\n");
+    }
+    if(a=10){
+        printf("Even this works\n");
+    }
+    if(0){
+        printf("Oh No");        //Zero as case or EXPRESSION Won't work... 
+    }
+    return 0;
+}
