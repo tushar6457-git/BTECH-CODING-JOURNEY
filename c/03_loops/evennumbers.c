c76d1a841ecbcbc76030bb63b1fde6418df54177
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-05-29 22:55:31 +0530

    Added evennumbers.c

---
+++ c/03_loops/evennumbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(int i = 2; i <= n; i += 2){
+         printf("%d\n",i);
+    }
+    
+    return 0;
+}
