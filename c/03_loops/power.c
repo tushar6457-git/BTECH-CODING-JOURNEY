32b7578f75ea699f918dc52278a2ae49fb779b00
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-06-14 21:05:28 +0530

    MTE: Loops is Over, Patter Printing Starts

---
+++ c/03_loops/power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter Base: ");
+    scanf("%d",&a);
+    printf("Enter Power: ");
+    scanf("%d",&b);
+    int power = 1;
+    for(int i = 1; i<=b; i++){
+        power = power*a;
+    }
+    printf("%d raised to the power %d is %d\n",a,b,power);
+    return 0;
+}
