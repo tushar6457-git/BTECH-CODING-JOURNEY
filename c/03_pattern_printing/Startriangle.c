59e9232c85f1dd46f23176b6a4230b0f323fe349
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-06-19 20:31:33 +0530

    Updation: StarTriangle PatternPrinting Practise

---
+++ c/03_pattern_printing/Startriangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){      //rows
+        for(int j=1; j<=i ; j++){       //columns
+            printf("* ");
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
