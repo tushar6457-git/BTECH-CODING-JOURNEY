0a508d024f4ed63fbf96675c2e1d75e579a73fac
tushar6457 <tusharbhardwaj6457@gmail.com>
2026-06-25 20:16:22 +0530

    Updation

--- c/03_pattern_printing/PlusStar.c
+++ c/03_pattern_printing/PlusStar.c
@@ -7,7 +7,7 @@ int main(){
 
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(j==3 || i==3){
+            if(j==n/2 || i==n/2){
                 printf("* ");
             }
             else{
